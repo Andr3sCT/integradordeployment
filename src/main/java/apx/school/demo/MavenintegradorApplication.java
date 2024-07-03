1fa92845738783ed7fa000dbba42d2da2edc53be
Andr3sCT <josecitalan@emilianisomascos.edu.gt>
2024-07-03 17:03:51 -0600

    primer commit

---
+++ src/main/java/apx/school/demo/MavenintegradorApplication.java
@@ -0,0 +1,13 @@
+package apx.school.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenintegradorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavenintegradorApplication.class, args);
+	}
+
+}
