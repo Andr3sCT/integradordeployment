5199509f2c8ec88aac66ab6d8e81ccb791361413
Andr3sCT <josecitalan@emilianisomascos.edu.gt>
2024-07-08 20:52:52 -0600

    first commit

--- src/main/java/apx/school/demo/MavenintegradorApplication.java
+++ src/main/java/apx/school/demo/MavenintegradorApplication.java
@@ -8,6 +8,6 @@ public class MavenintegradorApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MavenintegradorApplication.class, args);
+		System.out.println("El programa esta corriendo.");
 	}
-
 }
