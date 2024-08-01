901f494f6a1a9b4b3b082cfba7db1dc1bc19f54d
Andr3sCT <josecitalan@emilianisomascos.edu.gt>
2024-08-01 12:09:12 -0600

    Adición de Seguridad al Proyecto Integrador

--- src/main/java/apx/school/demo/Repository/MongoDBRepository.java
+++ src/main/java/apx/school/demo/Repository/MongoDBRepository.java
@@ -1,6 +1,10 @@
 package apx.school.demo.Repository;
-import apx.school.demo.Messages.Message;
-import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface MongoDBRepository extends MongoRepository<Message, String> {
+import apx.school.demo.Entity.AuthEntity;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import apx.school.demo.Model.User;
+
+
+public interface MongoDBRepository extends MongoRepository<User, String> {
+    User findByUsername(String username);
 }
